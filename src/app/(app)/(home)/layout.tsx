b9245520468a8f98a159f0a6c423b4483385b930
Ruslan Mykytyn <ruslan.mykytyn.03@gmail.com>
2025-07-13 14:09:24 +0300

    06-categories-finalization

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,6 +5,7 @@ import { Category } from "@/payload-types";
 
 import configPromise from "@payload-config";
 import { getPayload } from "payload";
+import { CustomCategory } from "./types";
 interface Props {
   children: React.ReactNode;
 }
@@ -23,11 +24,13 @@ const Layout = async ({ children }: Props) => {
         exists: false,
       },
     },
+
+    sort: "name",
   });
 
-  const formattedData = data.docs.map((doc) => ({
+  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
     ...doc,
-    subcategories: (doc.subcategories?.docs ?? []).map((doc: Category) => ({
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
       ...(doc as Category),
       subcategories: undefined,
     })),
