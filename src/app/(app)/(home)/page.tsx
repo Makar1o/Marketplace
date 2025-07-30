7dd475422cfd4391426a06926ac6c0c37066b599
Ruslan Mykytyn <ruslan.mykytyn.03@gmail.com>
2025-07-30 22:20:59 +0300

    08: authentification

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
+
 import { useTRPC } from "@/trpc/client";
 
 export default function Home() {
   const trpc = useTRPC();
-  const categories = useQuery(trpc.categories.getMany.queryOptions());
-
-  return <div>{JSON.stringify(categories.data, null, 2)}</div>;
+  const { data } = useQuery(trpc.auth.session.queryOptions());
+  return <div>{JSON.stringify(data?.user, null, 2)}</div>;
 }
