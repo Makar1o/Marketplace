d52b1fa805e7fdde016d8a2a2f4680639b0a4544
Ruslan Mykytyn <ruslan.mykytyn.03@gmail.com>
2025-08-28 18:33:09 +0300

    13-api-filters-sorting

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,11 +3,13 @@ import { createTRPCRouter } from "../init";
 
 import { categoriesRouter } from "@/module/categories/server/procedures";
 import { productsRouter } from "@/module/products/server/procedures";
+import { tagsRouter } from "@/module/tags/server/procedures";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   products: productsRouter,
   categories: categoriesRouter,
+  tags: tagsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
