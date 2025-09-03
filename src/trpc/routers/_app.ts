af5d9730d3f307ec9dbe40039ea51fe2bc78d98a
Ruslan Mykytyn <ruslan.mykytyn.03@gmail.com>
2025-09-03 23:05:48 +0300

    19: checkout page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,11 +5,13 @@ import { categoriesRouter } from "@/module/categories/server/procedures";
 import { productsRouter } from "@/module/products/server/procedures";
 import { tagsRouter } from "@/module/tags/server/procedures";
 import { tenantsRouter } from "@/module/tenants/server/procedures";
+import { checkoutRouter } from "@/module/checkout/server/procedures";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
   products: productsRouter,
   categories: categoriesRouter,
 });
